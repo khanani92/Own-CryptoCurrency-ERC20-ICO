ec6b3215f29372782d0a4888204febec674cd5d3
Muhammad-mudassir <muddassir_92@hotmail.com>
2018-10-22 19:49:51 +0500

    approve and transferFrom function added with tests

--- contracts/KhananiToken.sol
+++ contracts/KhananiToken.sol
@@ -15,7 +15,17 @@ contract KhananiToken {
         uint256  _value
     );
 
+    event Approval(
+        address indexed _owner,
+        address indexed _spender,
+        uint256  _value
+    );
+
+
     mapping(address => uint256) public balanceOf;
+    
+    //allance
+    mapping(address => mapping(address => uint256)) public allowance;
 
     constructor(uint256 _initalSupply) public {
         balanceOf[msg.sender] = _initalSupply;
@@ -39,6 +49,41 @@ contract KhananiToken {
         Transfer(msg.sender, _to, _value);
 
         //Return Boolean
+        return true;
+    }
+
+    //Delegated Transfer
+    
+    //Approve tranfer
+    function approve (address _spender, uint256 _value) public returns (bool success) {
+        //set the allowance
+        allowance[msg.sender][_spender] = _value;
+
+        //approve event
+        Approval(msg.sender, _spender, _value);
+        
+        
+        return true;
+    }
+
+    //TransferFrom
+    function transferFrom(address _from, address _to, uint256 _value) public returns (bool success) {
+        
+        require(_value <= balanceOf[_from] );
+
+        require(_value <= allowance[_from][msg.sender]);
+        
+        // //Transfer the balance
+         balanceOf[_from] -= _value;
+         balanceOf[_to] += _value;
+        
+        //update allowance
+        allowance[_from][msg.sender] -= _value;
+
+         //Transfer Event
+         Transfer(_from, _to, _value);
+
+        
         return true;
     }
 }
--- test/KhananiToken.js
+++ test/KhananiToken.js
@@ -34,7 +34,7 @@ contract('KhananiToken', function(accounts){
         return KhananiToken.deployed().then(function(instance){
             tokenInstance = instance;
             //Test 'require' statement first by transfaring something larger than sender's balance
-            //Method called with .call() don't actually call the function and create the transction
+            //Method called with .call() don't actually create the transition just call the function
             return tokenInstance.transfer.call(accounts[1], 99999999999999999999999);
         }).then(assert.fail).catch(function(error){
             //console.log('????????',typeof error,typeof error.message,error.message.indexOf('revert') >= 0)
@@ -59,4 +59,67 @@ contract('KhananiToken', function(accounts){
         })
     })
 
+    it('approve token for delegated Transfer',function(){
+        return KhananiToken.deployed().then(function(instance){
+            tokenInstance = instance;
+            //Method called with .call() don't actually create the transition just call the function
+            return tokenInstance.approve.call(accounts[1],100);
+        }).then(function(success){
+            assert.equal(success, true, 'it returns ture');
+            return tokenInstance.approve(accounts[1],100);
+        }).then(function(receipt){
+            assert.equal(receipt.logs.length, 1, 'trigger one event');
+            assert.equal(receipt.logs[0].event, 'Approval', 'should be a approval event');
+            assert.equal(receipt.logs[0].args._owner, accounts[0], 'logs the account tokens are authorized by');
+            assert.equal(receipt.logs[0].args._spender, accounts[1], 'logs the account tokens are authorized to');
+            assert.equal(receipt.logs[0].args._value, 100, 'logs the transfer amount');
+            return tokenInstance.allowance(accounts[0], accounts[1]);
+        }).then(function(allowance){
+            assert.equal(allowance.toNumber(), 100, 'stores the allowance for delegated transfer');
+        })
+    })
+
+    it('handles delegated token Transfer',function(){
+        return KhananiToken.deployed().then(function(instance){
+            tokenInstance = instance;
+            fromAccount = accounts[2];
+            toAccount = accounts[3];
+            spendingAccount = accounts[4];
+            return tokenInstance.transfer(fromAccount, 100, {from:accounts[0]}); //for testing giving token to other accounts
+        }).then(function(receipt){
+            return tokenInstance.approve(spendingAccount,10, {from:fromAccount});
+        }).then(function(receipt){
+            //Try trasferring smething larger than the sender's balance
+            return tokenInstance.transferFrom(fromAccount,toAccount, 9999999, {from:spendingAccount});
+        }).then(assert.fail).catch(function(error){
+            assert((error.message.indexOf('revert') >= 0), 'cannot transfer value larger than balance');
+            //Try trasferring smething larger than the aprroved amount
+            return tokenInstance.transferFrom(fromAccount,toAccount, 20, {from:spendingAccount});
+        }).then(assert.fail).catch(function(error){
+            assert((error.message.indexOf('revert') >= 0), 'cannot transfer valur larger than approved amount');
+            return tokenInstance.transferFrom.call(fromAccount,toAccount, 10, {from:spendingAccount});
+        })
+        .then(function(success){
+            assert.equal(success, true, 'it returns ture');
+            return tokenInstance.transferFrom(fromAccount,toAccount, 10, {from:spendingAccount});
+        }).then(function(receipt){
+            assert.equal(receipt.logs.length, 1, 'trigger one event');
+            assert.equal(receipt.logs[0].event, 'Transfer', 'should be a transfer event');
+            assert.equal(receipt.logs[0].args._from, fromAccount, 'logs the account tokens are transferred from');
+            assert.equal(receipt.logs[0].args._to, toAccount, 'logs the account tokens are transferred to');
+            assert.equal(receipt.logs[0].args._value, 10, 'logs the transfer amount');
+            return tokenInstance.balanceOf(fromAccount);
+        }).then(function(fromAccountBalance){
+            assert.equal(fromAccountBalance.toNumber(), 90, 'deducts amount from sender account')
+            return tokenInstance.balanceOf(toAccount);
+        }).then(function(toAccountBalance){
+            assert.equal(toAccountBalance.toNumber(), 10, 'add amount from sender account');
+            return tokenInstance.allowance(fromAccount, spendingAccount);
+        }).then(function(allowance){
+            assert.equal(allowance, 0, 'deducts the amount from allowance');
+        })
+        // .then(function(){
+        //     return tokenInstance.transferFrom(fromAccount,toAccount, 10, {from:spendingAccount});
+        // })
+    })
 })
