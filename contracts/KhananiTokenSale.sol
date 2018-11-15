199734658f548dba7e0561907f048975aeb5bcca
Muhammad-mudassir <muddassir_92@hotmail.com>
2018-11-15 18:08:32 +0500

    buy token function added with test

--- contracts/KhananiTokenSale.sol
+++ contracts/KhananiTokenSale.sol
@@ -4,8 +4,14 @@ import './KhananiToken.sol';
 
 contract KhananiTokenSale {
     address admin;
-    KhananiToken public tokenContract ;
+    KhananiToken public tokenContract;
     uint256 public tokenPrice;
+    uint256 public tokensSold;
+
+    event Sell(
+        address   _buyer,
+        uint256  _amount
+    );
 
     constructor (KhananiToken _tokenContract, uint256 _tokenPrice ) public {
         //Assign an Admin
@@ -17,4 +23,30 @@ contract KhananiTokenSale {
         //Set the token Price
         tokenPrice = _tokenPrice;
     }
+
+    //Multiple function 
+    function multiply(uint x, uint y) internal pure returns(uint z) {
+        require(y == 0 || (z = x * y) / y == x);
+    }
+
+    //Buy Token
+
+    function buyToken(uint256 _numberOfTokens) public payable {
+        //Require that value is equal to token
+        require(msg.value == multiply(_numberOfTokens , tokenPrice));
+
+        //require that enough token are avaible in token
+        require(tokenContract.balanceOf(this) == _numberOfTokens);        
+
+        //require transfer is succesfull
+        require(tokenContract.transfer(msg.sender, _numberOfTokens));        
+        
+        //Keep track of num of token sold
+        tokensSold += _numberOfTokens;
+
+
+        //trigger for Sell Event
+        Sell(msg.sender, _numberOfTokens);
+    }
+
 }
--- test/KhananiTokenSale.js
+++ test/KhananiTokenSale.js
@@ -1,9 +1,14 @@
+var KhananiToken = artifacts.require("./KhananiToken.sol");
 var KhananiTokenSale = artifacts.require("./KhananiTokenSale.sol");
 
 contract('KhananiTokenSale', function(accounts){
+    var tokenInstance;
     var tokenSaleInstance;
     var tokenPrice = 1000000000000000; // in wei
-
+    var admin = accounts[0];
+    var buyer = accounts[1];
+    var tokensAvailable = 750000;
+    var numberOfToken;
     it('initializes the contract with correct value',function(){
         return KhananiTokenSale.deployed().then(function(instance){
             tokenSaleInstance = instance;
@@ -23,4 +28,46 @@ contract('KhananiTokenSale', function(accounts){
         //     assert.equal(tokenStandard, 'Khanani Token v1.0');
         // })
     })
+
+    it('facilitates token buying',function(){
+        return KhananiToken.deployed().then(function(instance){
+            tokenInstance = instance;
+            return KhananiTokenSale.deployed();
+        }).then(function(SInstance){
+            tokenSaleInstance = SInstance;
+            //provision 75% of all token sale 
+            return tokenInstance.transfer(tokenSaleInstance.address, tokensAvailable, {from:admin} );
+        }).then(function(receipt){
+            numberOfToken = 10;
+            var value = numberOfToken * tokenPrice; 
+            return tokenSaleInstance.buyToken(numberOfToken,{from: buyer, value: value});
+        }).then(function(receipt){
+            assert.equal(receipt.logs.length, 1, 'trigger one event');
+            assert.equal(receipt.logs[0].event, 'Sell', 'should be a sell event');
+            assert.equal(receipt.logs[0].args._buyer, buyer, 'logs the account that purchased tokens');
+            assert.equal(receipt.logs[0].args._amount, numberOfToken, 'logs the number of token purchased');
+            return tokenSaleInstance.tokensSold();
+        }).then(function(amount){
+            assert.equal(amount.toNumber(), numberOfToken, 'increments the number of tokens sold');
+            return tokenInstance.balanceOf(buyer);
+        })
+        .then(function(buyerBalance){
+            console.log('buyer',buyerBalance.toNumber())
+            assert.equal(buyerBalance.toNumber(), numberOfToken)    
+            return tokenInstance.balanceOf(tokenSaleInstance.address);
+        })
+        .then(function(balance){
+            assert.equal(balance.toNumber(), tokensAvailable - numberOfToken)
+            //try to buy token diff from the ether value
+            return tokenSaleInstance.buyToken(numberOfToken,{from: buyer, value: 1});
+        }).then(assert.fail).catch(function(error){
+            assert(error.message.indexOf('revert') >= 0, 'msg.value must be equal number of tokens in wei')
+            return tokenSaleInstance.buyToken(800000,{from: buyer, value: numberOfToken * tokenPrice});
+        })
+        .then(assert.fail).catch(function(error){
+            //console.log(error);
+            assert(error.message.indexOf('revert') >= 0, '1 cannot purchase more tokens then available')
+        })
+    })
+    
 });
